159ead2fe95271e3d15554cbaa34ac851f07c4ff
Yuzunoha <studysteadystandby@gmail.com>
2022-08-15 20:10:17 +0900

    getStaticProps

--- pages/ssg.tsx
+++ pages/ssg.tsx
@@ -1,15 +1,16 @@
-// 型のために導入
-import { NextPage } from 'next';
-// Next.jsの組み込みのコンポーネント
+import { GetStaticProps, NextPage, NextPageContext } from 'next';
 import Head from 'next/head';
 
-// ページコンポーネントのpropsの型定義(ここでは空)
-type SSGProps = {};
+// ページコンポーネントのpropsの型定義
+type SSGProps = {
+  message: string;
+};
 
-// SSG向けのページを実装
-// NextPageはNext.jsのPages向けの型
-// NextPage<props>でpropsが入るPageであることを明示する
-const SSG: NextPage<SSGProps> = () => {
+// SSGはgetStaticPropsが返したpropsを受け取ることができる
+// NextPage<SSGProps>はmessage: stringのみを受け取って生成されるページの型
+const SSG: NextPage<SSGProps> = (props) => {
+  const { message } = props;
+
   return (
     <div>
       <Head>
@@ -18,9 +19,24 @@ const SSG: NextPage<SSGProps> = () => {
       </Head>
       <main>
         <p>このページは性的サイト生成(SSG)で出ている</p>
+        <p>{message}</p>
       </main>
     </div>
   );
+};
+
+// getStaticPropsはビルドに実行される
+// GetStaticProps<SSGProps>はSSGPropsを引数に取るgetStaticPropsの型
+export const getStaticProps: GetStaticProps<SSGProps> = async (context) => {
+  const timestamp = new Date().toLocaleString();
+  const message = `${timestamp} にgetStaticPropsが実行されました`;
+  console.log('message', message);
+  return {
+    // ここで返したpropsを元にページコンポーネントを描画する
+    props: {
+      message,
+    },
+  };
 };
 
 // ページコンポーネントのエクスポート方法はこれ
